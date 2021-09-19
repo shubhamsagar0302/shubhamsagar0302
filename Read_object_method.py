7bc1e5abf966c8d6a2be01b97e898c6c22931185
shubhamsagar0302 <88707608+shubhamsagar0302@users.noreply.github.com>
2021-09-19 15:40:20 +0530

    Delete Read_object_method.py

--- Read_object_method.py
+++
@@ -1,8 +0,0 @@
-try:
-    f=open("test.txt",'r',encoding='utf-8')
-    print(f.read())
-except:
-     print("operation not perform....")
-finally:
-    f=open("test.txt",'r',encoding="utf-8")
-    f.close()
